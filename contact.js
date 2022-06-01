03c08771ddcc73c54eb5145c42d4543d416cadf5
Hayden Muller <haydenmuller@gmail.com>
2022-06-01 14:37:11 +0200

    initial commit

---
+++ contact.js
@@ -0,0 +1,20 @@
+function contact() {
+    var script_url = "https://script.google.com/macros/s/AKfycbyew_Ff4KsGXMMnrmf32adr97beMYM_7CV0RtjZb-iDJoy0J2RYKrB-GqViE5oIa_s9/exec";
+
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var subject = $("#subject").val();
+    var message = $("#message").val();
+
+    var url = script_url + "?callback=thanks&name" + "&name=" + encodeURIComponent(name) + "&email=" + encodeURIComponent(email) + "&subject=" + encodeURIComponent(subject) + "&message=" + encodeURIComponent(message);
+    jQuery.ajax({
+        crossDomain: true,
+        url: url,
+        method: "GET",
+        dataType: "jsonp"
+});
+}
+
+function thanks(e) {
+    alert("Thanks, I'll reply as soon as I can.");
+}
